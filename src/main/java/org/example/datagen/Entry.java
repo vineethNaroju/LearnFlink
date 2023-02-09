d34e8d61b753dab368c0e1b7500a6e3f08f683bd
Vineeth N <narojv@amazon.com>
2023-02-10 05:01:45 +0530

    working file source with monitor continuously

---
+++ src/main/java/org/example/datagen/Entry.java
@@ -0,0 +1,31 @@
+package org.example.datagen;
+
+import org.apache.flink.streaming.api.functions.source.datagen.DataGeneratorSource;
+import org.apache.flink.streaming.api.functions.source.datagen.RandomGenerator;
+
+import java.util.Random;
+
+public class Entry {
+    public static void main(String[] args) {
+
+        RandomGenerator<Integer> randomGenerator = new RandomGenerator<Integer>() {
+            private final Random rand = new Random();
+
+            @Override
+            public Integer next() {
+                return rand.nextInt(1000);
+            }
+        };
+
+
+        DataGeneratorSource<Integer> source = new DataGeneratorSource<>(randomGenerator, 10,10000L);
+
+        /**
+
+         aah .... this source also uses rich parallel fn which doesn't have busy time enabled
+
+        **/
+
+
+    }
+}
---
+++ src/main/java/org/example/file/FileSourceEntry.java
@@ -0,0 +1,37 @@
+package org.example.file;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.time.Duration;
+
+public class FileSourceEntry {
+
+    public static void main(String[] args) throws Exception {
+
+
+        Path directory = new Path("/Users/narojv/input");
+
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.disableOperatorChaining();
+
+        final FileSource<String> fileSource = FileSource.
+                <String>forRecordStreamFormat(new TextLineInputFormat(), directory)
+                .monitorContinuously(Duration.ofSeconds(1L))
+                .build();
+
+        final DataStream<String> stream = env
+                .fromSource(fileSource, WatermarkStrategy.noWatermarks(), "FileSource");
+
+
+        stream.print();
+
+        env.execute();
+    }
+}
